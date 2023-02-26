d296be77536b53f2f48f2b832f3d9f23a50b82ae
hdml <hdml.github@gmail.com>
2023-02-26 13:30:18 +0100

    deps audit

--- init_git.sh
+++ init_git.sh
@@ -1,4 +1,4 @@
 # ssh-keygen -t ed25519 -C "hdml@gmail.com"
-git config core.sshCommand "$(which ssh) -i ~/.ssh/hdml"
+git config core.sshCommand "$(which ssh) -i ~/.ssh/hdml.github"
 git config user.name "hdml"
-git config user.email "hdml@gmail.com"
+git config user.email "hdml.github@gmail.com"
