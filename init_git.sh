9c0949f90c294a8d29b94099dff60efe73673097
hdml <hdml.github@gmail.com>
2024-03-24 16:42:31 +0100

    git init script

--- init_git.sh
+++
@@ -1,5 +0,0 @@
-# ssh-keygen -t ed25519 -C "hdml.github@gmail.com"
-# ssh-agent bash -c 'ssh-add ~/.ssh/hdml.github; git clone git@github.com:hdml-github/hdml.git'
-git config core.sshCommand "$(which ssh) -i ~/.ssh/hdml.github"
-git config user.name "hdml"
-git config user.email "hdml.github@gmail.com"
